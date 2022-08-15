6b36a932a5e2c3f873bd8a00d66c5a9708aecbab
Gayu-2294 <gayu221094@gmail.com>
2022-08-15 20:44:05 +0530

    done my coding

--- src/simple/Simple.java
+++ src/simple/Simple.java
@@ -2,7 +2,12 @@ package simple;
 
 public class Simple {
 private void object3() {
-System.out.println("object3");
+System.out.println("object3");//master
 
 }
+private void object4() {
+	System.out.println("object4");//gayathri
+	
 }
+}
+
